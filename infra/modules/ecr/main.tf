bdeb377750065122ab22c8cae11f9d1ba3472ec1
malli407 <42290526+malli407@users.noreply.github.com>
2025-12-19 14:43:49 +0530

    Add files via upload

---
+++ infra/modules/ecr/main.tf
@@ -0,0 +1,58 @@
+resource "aws_ecr_repository" "this" {
+  for_each = toset(var.repositories)
+
+  name                 = each.value
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name        = each.value
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  for_each = toset(var.repositories)
+
+  repository = aws_ecr_repository.this[each.value].name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["v"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 10
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Remove untagged images after 7 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 7
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
