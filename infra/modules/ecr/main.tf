1a5c93e02f0d3e375db645f5e81831f2438953f3
malli407 <42290526+malli407@users.noreply.github.com>
2025-12-20 21:22:43 +0530

    Update main.tf

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,58 +1,15 @@
+variable "repositories" { type = list(string) }
+
 resource "aws_ecr_repository" "this" {
   for_each = toset(var.repositories)
-
-  name                 = each.value
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
-
-  tags = {
-    Name        = each.value
-    Project     = var.project_name
-    Environment = var.environment
-  }
+  name     = each.key
+  force_delete = true
+  image_scanning_configuration { scan_on_push = true }
+  encryption_configuration { encryption_type = "AES256" }
+  tags = { Name = each.key }
 }
 
-resource "aws_ecr_lifecycle_policy" "this" {
-  for_each = toset(var.repositories)
-
-  repository = aws_ecr_repository.this[each.value].name
-
-  policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "Keep last 10 images"
-        selection = {
-          tagStatus     = "tagged"
-          tagPrefixList = ["v"]
-          countType     = "imageCountMoreThan"
-          countNumber   = 10
-        }
-        action = {
-          type = "expire"
-        }
-      },
-      {
-        rulePriority = 2
-        description  = "Remove untagged images after 7 days"
-        selection = {
-          tagStatus   = "untagged"
-          countType   = "sinceImagePushed"
-          countUnit   = "days"
-          countNumber = 7
-        }
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
+output "repository_urls" {
+  value = { for k, r in aws_ecr_repository.this : k => r.repository_url }
 }
 
